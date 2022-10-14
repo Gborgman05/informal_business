40aa0001d25579a56fd6d0f09721a3263d75bfef
Gborgman05 <gborgman@calpoly.edu>
2022-10-13 22:07:40 -0700

    exploratory

--- pull_img.py
+++ pull_img.py
@@ -1,5 +1,40 @@
+# python3 -m pip install google_streetview
+# Import google_streetview for the api module
+import google_streetview.api
+import google_streetview.helpers
+
+# Define parameters for street view api
+key = 'mykey'
+
+def gen_coords(south_west, north_east, intervals):
+    west,south = south_west
+    east,north= north_east
+    lat = [l for l in range(south, north, abs(north - south) / intervals)]
+    lon = [l for l in range(west, east, abs(east - west) / interval)]
+    
 def main():
-    pass
+    # Define parameters for street view api
+    params = [{
+    'size': '600x300', # max 640x640 pixels
+    'location': '46.414382,10.013988',
+    'heading': '151.78',
+    'pitch': '-0.76',
+    'key': key
+    }]
+
+    # Create a results object
+    results = google_streetview.api.results(params)
+
+    # Preview results
+    # Download images to directory 'downloads'
+    results.download_links('downloads')
+
+    # Save links
+    results.save_links('links.txt')
+
+    # Save metadata
+    results.save_metadata('metadata.json')
 
 if __name__ == "__main__":
-    main()
+    # main()
+    print(gen_coords((17.335162, 78.366367), (17.450953, 78.459989), 3))
